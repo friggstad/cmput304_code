28d50da44dd092107b692d5e94c303df2c0a759d
friggstad <zac.friggstad@gmail.com>
2021-09-10 11:52:54 -0600

    Update obst.py

--- dynamic_programming/obst/obst.py
+++ dynamic_programming/obst/obst.py
@@ -20,8 +20,9 @@ def obst(i, j):
 if __name__ == "__main__":
     n = int(input())
 
-    # recursion depth <= since each recursive call is
-    # to a strictly smaller interval
+    # recursion depth is <= n since each recursive call is
+    # to a strictly smaller interval, but Python has a few function calls
+    # on the stack (eg. the min() function) as well. 5*n turns out to be ok.
     sys.setrecursionlimit(5*n)
 
     # the extra 0.0 is a dummy entry so we can 1-index
